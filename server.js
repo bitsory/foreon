ef68740fa73aa306ce8826b19ed6dbb39be52529
bitsory <rangdad@gmail.com>
2023-05-26 11:34:35 -0400

    product process

--- server.js
+++ server.js
@@ -75,27 +75,25 @@ app.use(session({
 
 app.use(cookieParser("secret"));
   
-app.get('/',(req,res) => { 
-    console.log("home home home");
-    
-    const axios = require('axios');
+
+const axios = require('axios');
 
 // Config Set Up
 // const targetEnv = 'https://sandbox.dev.clover.com'; // Pointing to Sandbox Environment
-// const targetEnv = 'https://www.clover.com'; // Pointing to Prod Environment
-const targetEnv = 'https://api.clover.com'; // Pointing to Prod Environment
+const targetEnv = 'https://www.clover.com'; // Pointing to Prod Environment
 
 const appID = process.env.PRODUCTAPPID; // Input your app ID here
 const appSecret = process.env.PRODUCTAPPSECRET; // Input your app secret here
 
 app.get('/', (req, res) => authenticate(req, res));
 
+console.log("get api hello")
 // Steps 1 & 2 - Request merchant authorization to receive authorization code
 const authenticate = async (req, res) => {
   const url = `${targetEnv}/oauth/authorize?client_id=${appID}`;
 
   // If there is no code parameter in the query string of the current url
-  //redirect user for authentication. If there isn't then request API token 
+ // redirect user for authentication. If there isn't then request API token 
   !req.query.code ? await res.redirect(url) : await requestAPIToken(res, req.query);
 }
 
@@ -110,6 +108,7 @@ const requestAPIToken = async (res, query) => {
 }
 
 
+
 /*
     if (req.session.loginData && req.session.loginData.id == "cafeforeadmin") {
         res.render('admin.ejs', {post : "ADMIN"});
@@ -121,7 +120,7 @@ const requestAPIToken = async (res, query) => {
 		res.sendFile(__dirname + "/public/index.html");
 	}
 */  
-});
+
 
 
 
